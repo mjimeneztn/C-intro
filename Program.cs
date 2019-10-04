7e31c6f2a623d08643de771e0519365fbf8d2175
mjimeneztn <mjimeneztn@gmail.com>
2019-10-04 12:07:51 -0500

    class practice from yesterday

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bangazoncli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
